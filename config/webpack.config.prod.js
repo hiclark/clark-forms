e3b408602c622db62b768448e02001b98b91e9ae
Willis Plummer <willisplummer@gmail.com>
2018-03-05 18:36:33 -0500

    prettier

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const path = require('path');
 const webpack = require('webpack');
 const eslintFormatter = require('react-dev-utils/eslintFormatter');
@@ -199,8 +197,8 @@ module.exports = {
   ],
   // CRL: added externals block for library
   externals: {
-    'react': 'react',
-    'react-dom': 'react-dom'
+    react: 'react',
+    'react-dom': 'react-dom',
   },
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
