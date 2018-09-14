89747746fa2d2410f6720ba70323695ba15bf3f2
Willis Plummer <willisplummer@gmail.com>
2018-09-14 16:15:24 -0400

    mark styled-components as external

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -199,6 +199,11 @@ module.exports = {
   externals: {
     react: 'react',
     'react-dom': 'react-dom',
+    'styled-components': {
+      commonjs: 'styled-components',
+      commonjs2: 'styled-components',
+      amd: 'styled-components',
+    },
   },
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
