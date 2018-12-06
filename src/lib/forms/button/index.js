e5207af494f47c1df832358a055b68bec61014c8
Jessica <jessica@hiclark.com>
2018-12-06 14:52:43 -0500

    Extract styles

--- src/lib/forms/button/index.js
+++ src/lib/forms/button/index.js
@@ -1,24 +1,9 @@
 // @flow
 
 import React, { type Node } from 'react';
-import { Link, type LocationShape } from 'react-router-dom';
-import styled, { css } from 'styled-components';
+import { type LocationShape } from 'react-router-dom';
 
-import {
-  BUTTON_COLOR_PRIMARY,
-  BUTTON_COLOR_TERTIARY,
-  GREY_10,
-  GREY_25,
-  GREY_75,
-  LINK_COLOR_QUINARY,
-  WHITE,
-} from '../../styles/colors';
-import { BORDER_RADIUS_F2 } from '../../styles/border-radius';
-import { BORDER_WIDTH_1 } from '../../styles/borders';
-import { SPACING_MEDIUM } from '../../styles/spacing';
-import { TYPE_SCALE_F6 } from '../../styles/type-scale';
-import { FONT_WEIGHT_700 } from '../../styles/font-weight';
-import { Z_INDEX_1 } from '../../styles/z-index';
+import { Container, Copy, SecondaryIcon, StyledButton, StyledLink } from './styles';
 
 type PropsType = {
   children: Node,
@@ -44,7 +29,8 @@ const Button = ({ children, disabled, icon, onClick, path, styleType, type }: Pr
         styleType={styleType}
         type={type}
       >
-        {children}
+        {icon && <SecondaryIcon>{icon}</SecondaryIcon>}
+        <Copy>{children}</Copy>
       </StyledButton>
     )}
   </Container>
@@ -60,94 +46,3 @@ Button.defaultProps = {
 };
 
 export default Button;
-
-const MAX_WIDTH = '17.5rem';
-const Container = styled.span`
-  z-index: 0;
-  position: relative;
-  width: 100%;
-`;
-
-const primary = disabled => css`
-  background: ${disabled ? GREY_25 : BUTTON_COLOR_PRIMARY};
-  box-shadow: ${props => (props.disabled ? '' : '0 2px 8px 0 rgba(234, 73, 0, 0.2)')};
-  border: 0;
-  color: ${WHITE};
-
-  &:hover {
-    background: ${disabled ? GREY_25 : BUTTON_COLOR_TERTIARY};
-  }
-`;
-
-const secondary = disabled => css`
-  background: ${WHITE};
-  border: ${disabled ? `${BORDER_WIDTH_1} solid ${GREY_10}` : `${BORDER_WIDTH_1} solid ${GREY_25}`};
-  color: ${disabled ? GREY_25 : GREY_75};
-
-  &:hover {
-    border: ${disabled
-      ? `${BORDER_WIDTH_1} solid ${GREY_10}`
-      : `${BORDER_WIDTH_1} solid ${LINK_COLOR_QUINARY}`};
-    color: ${disabled ? GREY_25 : GREY_75};
-  }
-`;
-
-const tertiary = css`
-  color: ${LINK_COLOR_QUINARY};
-  text-transform: capitalize;
-  padding: 0;
-  border: 0;
-
-  &:hover {
-    text-decoration: underline;
-  }
-`;
-
-const dashed = disabled => css`
-  background: ${WHITE};
-  border: ${disabled
-    ? `${BORDER_WIDTH_1} dashed ${GREY_10}`
-    : `${BORDER_WIDTH_1} dashed ${GREY_25}`};
-  color: ${disabled ? GREY_10 : GREY_75};
-
-  &:hover {
-    border: ${disabled
-      ? `${BORDER_WIDTH_1} dashed ${GREY_10}`
-      : `${BORDER_WIDTH_1} dashed ${LINK_COLOR_QUINARY}`};
-    color: ${disabled ? GREY_10 : GREY_75};
-  }
-`;
-
-const buttonStyleType = disabled => ({
-  primary: () => primary(disabled),
-  secondary: () => secondary(disabled),
-  dashed: () => dashed(disabled),
-  tertiary,
-});
-
-const StyledButton = styled.button`
-  ${FONT_WEIGHT_700};
-  ${TYPE_SCALE_F6};
-  ${BORDER_RADIUS_F2};
-  ${Z_INDEX_1};
-  padding: ${SPACING_MEDIUM};
-  max-width: ${MAX_WIDTH};
-  width: 100%;
-  height: auto;
-  align-items: center;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  text-decoration: none;
-  text-transform: uppercase;
-
-  ${({ disabled }) => disabled && 'pointer-events: none;'};
-  ${({ disabled, styleType }) => buttonStyleType(disabled)[styleType]};
-
-  svg {
-    height: ${SPACING_MEDIUM};
-    width: ${SPACING_MEDIUM};
-  }
-`;
-
-const StyledLink = StyledButton.withComponent(Link);
