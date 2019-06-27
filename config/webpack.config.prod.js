3eff5658835628ee8bca1e65c24691725790375d
Willis Plummer <willisplummer@gmail.com>
2019-06-27 08:59:12 -0400

    moment to peer dep

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -207,6 +207,7 @@ module.exports = {
     'styled-components': 'styled-components',
     'redux-form': 'redux-form',
     'clark-styles': 'clark-styles',
+    moment: 'moment',
   },
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
